1f51167021a01f23d4b313074bcf78f519243534
Leonardo-Amengual <leonardo.amengual@outlook.com>
2025-06-08 15:24:38 -0300

    Passando o id do cliente para a pagina de edição

--- src/app/consulta/consulta.component.ts
+++ src/app/consulta/consulta.component.ts
@@ -9,6 +9,7 @@ import { MatTableModule } from '@angular/material/table'
 import { MatButtonModule } from '@angular/material/button'
 import { ClienteService } from '../cliente.service';
 import { Cliente } from '../cadastro/cliente';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-consulta',
@@ -29,9 +30,12 @@ export class ConsultaComponent {
 
   nomeBusca: string = '';
   listaCliente: Cliente[] = [];
-  colunasTable: string[] = ["id", "nome", "cpf", "dataNascimento", "email"]
+  colunasTable: string[] = ["id", "nome", "cpf", "dataNascimento", "email", "acoes"]
 
-  constructor(private service: ClienteService) {
+  constructor(
+    private service: ClienteService,
+    private router: Router
+  ) {
 
   }
 
@@ -41,6 +45,10 @@ export class ConsultaComponent {
 
   pesquisar() {
     this.listaCliente = this.service.pesquisarCliente(this.nomeBusca)
+  }
+
+  preparaEditar(id: string) {
+    this.router.navigate(['/cadastro'], { queryParams: { "id": id } })
   }
 
 }
